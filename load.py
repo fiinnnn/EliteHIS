baad3c40a80e5ec812957061f99ea7c4aaf8c6e1
Finn Vos <finnvos34@protonmail.ch>
2018-03-18 15:03:24 +0100

    Add estimated distance calculation

--- load.py
+++ load.py
@@ -6,7 +6,7 @@ import Tkinter as tk
 import myNotebook as nb
 from config import config
 
-import heading
+import utils
 import overlay
 
 this = sys.modules[__name__]
@@ -15,9 +15,10 @@ def plugin_start():
     """
     Load EliteHIS plugin
     """
-    #Initialize target coordinates
-    this.targetLat = tk.DoubleVar(value=-29.1664)
-    this.targetLong = tk.DoubleVar(value=-30.5041)
+    #Initialize target coordinates and planet radius
+    this.targetLat = tk.DoubleVar(value=0)
+    this.targetLong = tk.DoubleVar(value=0)
+    this.planetRadius = tk.IntVar(value=0)
 
     print("EliteHIS: loaded")
     return "EliteHIS"
@@ -42,6 +43,8 @@ def plugin_prefs(parent, cmdr, is_beta):
     nb.Label(frame, text="Longitude").grid()
     nb.Entry(frame, textvariable=this.targetLong).grid()
 
+    nb.Label(frame, text="Planet radius (in metres)").grid()
+    nb.Entry(frame, textvariable=this.planetRadius).grid()
     return frame
 
 def dashboard_entry(cmdr, is_beta, entry):
@@ -50,15 +53,20 @@ def dashboard_entry(cmdr, is_beta, entry):
     """
     #Lat/Long not always in status.json
     try:
-        currentLat = entry['Latitude']
-        currentLong = entry['Longitude']
-        recalculate_heading(currentLat, currentLong)
+        currentLat = entry["Latitude"]
+        currentLong = entry["Longitude"]
+        altitude = entry["Altitude"]
+        recalculate_info(currentLat, currentLong, altitude)
     except KeyError:
         pass
 
-def recalculate_heading(currentLat, currentLong):
+def recalculate_info(currentLat, currentLong, altitude):
     """
     Recalculate target heading
     """
-    targetHeading = heading.calc_heading(currentLat, currentLong, this.targetLat.get(), this.targetLong.get())
-    overlay.show_heading(targetHeading)
+    heading = utils.calc_heading(currentLat, currentLong, this.targetLat.get(), this.targetLong.get())
+    overlay.show_heading(heading)
+    
+    if this.planetRadius.get() > 0:
+        distance = utils.calc_distance(currentLat, currentLong, this.targetLat.get(), this.targetLong.get(), this.planetRadius.get(), altitude)
+        overlay.show_distance(distance)
--- overlay.py
+++ overlay.py
@@ -14,7 +14,19 @@ this.overlay = edmcoverlay.Overlay()
 
 def show_heading(heading):
     this.overlay.send_message("heading", "Heading: " + str(heading), "#ff7100", 2, 2, 8, "large")
-    #this.overlay.send_message("distance", "Distance: ", "#ff7100", 2, 25, 8, "large")
+    
+def show_distance(distance):
+    unit = "m"
+
+    if (distance > 1000):
+        distance = distance / 1000
+        unit = "km"
+
+    if (distance > 1000):
+        distance = distance / 1000
+        unit = "Mm"
+
+    this.overlay.send_message("distance", "Distance: " + str(round(distance, 2)) + unit, "#ff7100", 2, 30, 8, "large")
 
 def shutdown_overlay():
     edmcoverlay.shutdown()
